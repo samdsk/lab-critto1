784e7d43c3945ef83fe172b8d1dbe196a46f1b07
Sam <sam@SamMacBook.local>
2020-11-26 20:28:59 +0100

    Es. 1 Solved

--- rsa.c
+++ rsa.c
@@ -19,15 +19,19 @@ void RSA_D(
     unsigned long long lq  
 );
 
-mpz_t inv_mul(mpz_t e, mpz_t p, mpz_t q);
+void inv_mul(mpz_t d,mpz_t e, mpz_t p, mpz_t q);
 
 int main(){
     unsigned long long lm = 30120;
     unsigned long long le = 9007;
     unsigned long long lp = 885320963;
     unsigned long long lq = 238855417;
-    mpz_t ctxt = RSA_E(lm,le,lp,lq);
-    mpz_t m = RSA_D(ctxt,le,lp,lq);
+
+    mpz_t m,ctxt;
+    mpz_init(m);
+    mpz_init(ctxt);
+    RSA_E(ctxt,lm,le,lp,lq);
+    RSA_D(m,ctxt,le,lp,lq);
 
     mpz_clear(ctxt);
     mpz_clear(m);
@@ -72,35 +76,49 @@ void RSA_D(
     unsigned long long lp,
     unsigned long long lq){
 
-    mpz_t m,E,P,Q,n,d;    
-    mpz_inits(m,E,P,Q,n,d,(mpz_ptr) NULL);
-    d = inv_mul(E,P,Q);
+    mpz_t E,P,Q,n,d,phi;    
+    mpz_inits(E,P,Q,n,d,phi,(mpz_ptr) NULL);
+
+    mpz_set_ui(E,le);
+    mpz_set_ui(P,lp);
+    mpz_set_ui(Q,lq);
+    mpz_mul(n,P,Q);
+  
+    mpz_set_ui(d,1);
+    mpz_sub_ui(P,P,1);
+    mpz_sub_ui(Q,Q,1);    
+    mpz_mul(phi,P,Q);
+
+    mpz_invert(d,E,phi);
     mpz_powm(m,ctxt,d,n);
 
     printf("m: ");
     mpz_out_str(stdout,10,m);
+    printf(" d:");
+    mpz_out_str(stdout,10,d);
     printf("\n");
 
     mpz_clear(E);
     mpz_clear(P);
     mpz_clear(Q);
-    mpz_clear(ctxt);
+    //mpz_clear(ctxt);
     mpz_clear(n);
     mpz_clear(d);
 
     return;
 }
 
-mpz_t inv_mul(mpz_t e,mpz_t p, mpz_t q){
-    mpz_t d;
+void inv_mul(mpz_t d,mpz_t e,mpz_t p, mpz_t q){
+    
     mpz_t r;
     mpz_t phi;
-    mpz_init(d);
+    //mpz_init(d);
     mpz_init(r);
     mpz_init(phi);
     mpz_set_ui(d,1);
-
-    mpz_mul(phi,mpz_sub_ui(p,1),mpz_sub_ui(q,1));
+    mpz_sub_ui(p,p,1);
+    mpz_sub_ui(q,q,1);
+    mpz_mul(phi,p,q);
     
 
     while(1){
@@ -113,7 +131,7 @@ mpz_t inv_mul(mpz_t e,mpz_t p, mpz_t q){
         mpz_out_str(stdout,10,d);
         printf("\n");
 
-        mpz_add_ui(d,1);
+        mpz_add_ui(d,d,1);
     }
 
     mpz_clear(r);
@@ -122,5 +140,5 @@ mpz_t inv_mul(mpz_t e,mpz_t p, mpz_t q){
     mpz_clear(e);
     mpz_clear(q);
 
-    return d;
+    return;
 }
